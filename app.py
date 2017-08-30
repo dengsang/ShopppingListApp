a204ec8ae0af99bead10466a3671152b7173d58b
dengsang <dengsangt@gmail.com>
2017-08-30 14:31:05 +0300

    update login and register module app

--- app.py
+++ app.py
@@ -1,30 +1,7 @@
-from flask import Flask, render_template, request, redirect, Response, abort, session, url_for
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from flask import Flask, render_template, request, redirect, flash, url_for
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 app = Flask(__name__)
-
-
-#configararion
-app.config.update(
-    DEBUG=True,
-    SECRET_KEY='secret_xxx'
-)
-
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
-
-
-#crazyusermodel
-class User(UserMixin):
-    def __init__(self, id):
-        self.id = id
-        self.name = "user" + str(id)
-        self.password = self.name + "_secret"
-
-    def __repr__(self):
-        return "%d/%s/%s" % (self.id, self.name, self.password)
 
 
 @app.route('/')
@@ -33,63 +10,50 @@ def index():
     return render_template('index.html', title="homepage")
 
 
-@app.route('/signup')
+class User:
+    def __init__(self):
+        self.username = []
+        self.email = []
+        self.password = []
+
+    def db(self):
+        user = {}
+        db_session = {'username:', 'email:', 'password:'}
+        db_session.update(user)
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+
+
+# form.username.data, form.email.data,form.password.data
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    db_session = {'username:', 'email:', 'password:'}
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User()
+        db_session.update(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
 
 
 @app.route('/dashboard')
-@login_required
 def dashboard():
     return render_template('dashboard.html')
 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if password == username + "_secret":
-            id = username.split('user')[1]
-            user = User(id)
-            login_user(user)
-            return redirect(request.args.get("next"))
-        else:
-            return abort(401)
-    else:
-        return Response('''
-        <html>
-            <head>
-             <link href="../static/css/bootstrap.min.css" rel="stylesheet" media="screen">
-            </head>
-        <body>
-        <div class="container">
-        <h1>Please login</h1>
-        <br>
-            <form action="" method="post">
-                <p><input type=text name=username value={{
-          request.form.username }}>
-                <p><input type=password name=password value={{
-          request.form.password }}>
-                <p><input type=submit value=Login>
-            </form>
-            </div>
-        </body>
-        </html>
-            ''')
-
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return Response('<p>Logged out</p>')
-
-
-#A callback to reload
-@login_manager.user_loader
-def load_user(userid):
-    return User(userid)
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
