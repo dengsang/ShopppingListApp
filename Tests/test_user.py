46cf4b7a76179cecce9aab81a8c76c200272ad95
dengsang <dengsangt@gmail.com>
2017-09-01 06:52:20 +0300

    Adding test python that test on users added to the list and there login detailse

--- Tests/test_user.py
+++ Tests/test_user.py
@@ -1,9 +1,23 @@
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class UserTestCase(unittest.TestCase):
+    def test_db(self):
+        self.users = []
+        self.user_names = []
+        if self.user_names and self.users is list:
+            self.assertIn(self.user_names, self.users)
+            self.assertListEqual(self.users.append(self.user_names), self.users)
+            return "Item added to the list successfully"
+
+    def test_user_login(self, email, password):
+        self.email = email
+        self.password = password
+        self.emails = [email, password]
+        if email in self.emails:
+            if password == password:
+                self.assertEqual(password, password, msg='Login and redirect to dashboard')
+                return 'successful login'
 
 
 if __name__ == '__main__':
