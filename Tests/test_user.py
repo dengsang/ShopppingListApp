148519055d476a4d2eedc7f7abc6ae52b287999c
dengsang <dengsangt@gmail.com>
2017-09-06 07:42:34 +0300

    update test_user.py file adding more test

--- Tests/test_user.py
+++ Tests/test_user.py
@@ -1,7 +1,23 @@
 import unittest
+from Tests.basetest import TestCase
+from app import User
+import app
 
 
 class UserTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        # os.path.join(app.config['BASEDIR'])
+
+        self.app = app.test_client()
+
+        self.assertEquals(app.debug, False)
+
+    def tearDown(self):
+        pass
+
     def test_db(self):
         self.users = []
         self.user_names = []
@@ -13,7 +29,7 @@ class UserTestCase(unittest.TestCase):
     def test_user_login(self, email, password):
         self.email = email
         self.password = password
-        self.emails = [email, password]
+        self.emails = dict(email=email, password=password)
         if email in self.emails:
             if password == password:
                 self.assertEqual(password, password, msg='Login and redirect to dashboard')
