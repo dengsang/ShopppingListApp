50c281b067656579c3e030f4ad0b66f5ee2b3ef1
dengsang <dengsangt@gmail.com>
2017-09-14 08:09:28 +0300

    separate the views and the forms from the main app

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
-from wtforms import Form, StringField, PasswordField, validators, IntegerField
-from wtforms.validators import Email
+from forms import RegistrationForm, TelephoneForm, EmailPasswordForm
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
+app.config['SECRET_KEY'] = b'\x13\x82\xe8\xf8\rQ\x168\x11\x7fs\x1a\xcaA/k\x83\x18\x19\xe1g?\xf4@\xd1'
 app.config['DEBUG'] = True
 users = []
 items = []
 
 
 class User:
-    def __init__(self, username, email, password):
+    def __init__(self):
         global users
-        self.username = username
-        self.email = email
-        self.password = password
+        # self.username = username
+        # self.email = email
+        # self.password = password
 
     def db(self, user_obj):
         user_names = [user["username"] for user in users]
@@ -37,23 +36,23 @@ class User:
 
 
 class AddList:
-    def __init__(self, item, quantity, price):
+    def __init__(self):
         global items
-        self.item = item
-        self.quantity = quantity
-        self.price = price
+        # self.item = item
+        # self.quantity = quantity
+        # self.price = price
 
     def add_list(self, item_obj):
-        user_items = [item["items"] for item in items]
-        if item_obj["items"] in user_items:
+        user_items = [item["item"] for item in items]
+        if item_obj["item"] in user_items:
             return "Item already in a list"
         items.append(item_obj)
         # print(items)
         return "success"
 
     def delete_item(self, item_obj):
-        user_items = [item["items"] for item in items]
-        if item_obj["items"] in user_items:
+        user_items = [item["item"] for item in items]
+        if item_obj["item"] in user_items:
             items.clear()
             flash('Item deleted')
             return "success"
@@ -63,16 +62,6 @@ class AddList:
 @app.route('/index')
 def index():
     return render_template('index.html', title="homepage")
-
-
-class RegistrationForm(Form):
-    username = StringField('username', [validators.Length(min=4, max=25)])
-    email = StringField('email', [validators.Length(min=6, max=35)])
-    password = PasswordField('password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('confirm')
 
 
 # form.username.data, form.email.data,form.password.data
@@ -92,18 +81,12 @@ def signup():
     return render_template('signup.html', form=form)
 
 
-class TelephoneForm(Form):
-    items = StringField('items', [validators.required()])
-    quantity = IntegerField('quantity', [validators.required()])
-    price = IntegerField('price')
-
-
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
     form = TelephoneForm(request.form)
     if request.method == 'POST' and form.validate():
         user = AddList()
-        db_list = {'items': form.items.data, 'quantity': form.quantity.data, 'price': form.price.data}
+        db_list = {'item': form.item.data, 'quantity': form.quantity.data, 'price': form.price.data}
         item_list = user.add_list(db_list)
         # print(item_list)
         if item_list == "success":
@@ -113,24 +96,19 @@ def dashboard():
     return render_template('dashboard.html', form=form, data=items)
 
 
-@app.route('/delete', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def delete():
     form = TelephoneForm(request.form)
     if request.method == 'POST' and form.validate():
         user = AddList()
-        db_list = {'items': form.items.data, 'quantity': form.quantity.data, 'price': form.price.data}
+        db_list = {'item': form.item.data, 'quantity': form.quantity.data, 'price': form.price.data}
         item_list = user.delete_item(db_list)
         # print(item_list)
         if item_list == "success":
-            flash('Item Added Successfully')
+            flash("Item Added Successfully")
             return redirect(url_for('dashboard'))
         return redirect(url_for('dashboard'))
     return render_template('dashboard.html', form=form, data=items)
-
-
-class EmailPasswordForm(Form):
-    email = StringField('email', validators=[Email()])
-    password = PasswordField('password', validators=[])
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -153,7 +131,7 @@ def logout():
 
 @app.route('/list_form')
 def list_form():
-    return render_template('add_list.html')
+    return render_template('add_list.html', data=items)
 
 
 if __name__ == '__main__':
